3ee1812075b2f37d35f382fb9b72c8012f7e3a4d
Felipe Correa <eu@felps.dev>
2025-01-13 23:46:31 -0300

    Tray and notifications example

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, Menu, Tray, Notification, nativeImage } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
@@ -42,6 +42,25 @@ app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
+  // Setup Tray
+  const trayIcon = nativeImage.createFromDataURL(
+    'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/wAALCAAUABQBAREA/8QAFwABAQEBAAAAAAAAAAAAAAAACAUHBv/EACcQAAEDBAEDBAMBAAAAAAAAAAECAwQFBgcRCAASIRQiMVEJI0Fh/9oACAEBAAA/ACbxW4SI5HYvvfKtZyvSrJo1myo8d+TPjlxpDWg7KfdV3J7UtsbKEjZWshJKANnQbf4UcXc9Q6rbHFPk9OuHINKiOTGKLcVFVT2qulse4R3FJQU/Y3368dwSnawIp8CZS50im1GM5HlRHVsPsuJ7VtuJUUqSoH4IIII/zphcKrXpGdsN5W4yw8m1O2r1uFEerWzTHqt6ek1t1kpLkd5vXvc/U3rzvSu4AhtXWjcNfx5Z8szPlCvHNKpeMKPSZLzEWZHrsZqo1Ka4w6hEaEplaySQVKWdaLaVgfJIFWdk04ZuyCKTWHatBF01X0092QJC5bXq3O15ToADhWNKKwPcTv8AvXEMvPR3UPsOrbcbUFoWhRCkqB2CCPII++rVZvy9rjcjvXBeFbqbkRKkR1zKg8+plKhohBWo9oI8HWvHUL56/9k='
+  )
+  const tray = new Tray(trayIcon)
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Hello from Numa!',
+      click: (): void => {
+        return new Notification({
+          title: 'Numa App',
+          body: 'Hello!'
+        }).show()
+      }
+    }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
@@ -50,7 +69,12 @@ app.whenReady().then(() => {
   })
 
   // IPC test /w tray
-  ipcMain.on('ping', () => console.log('pong'))
+  ipcMain.on('ping', () => {
+    new Notification({
+      title: 'Numa App',
+      body: 'Pong from Numa!'
+    }).show()
+  })
 
   createWindow()
 
